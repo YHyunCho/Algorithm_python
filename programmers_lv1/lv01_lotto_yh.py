d2ce0141c0c653bc3beabb5dd72e6fa05a29f658
YHyunCho <yhwmb321@gmail.com>
2022-07-05 22:57:07 +0900

    commit

--- programmers_lv1/lv01_lotto_yh.py
+++ programmers_lv1/lv01_lotto_yh.py
@@ -12,28 +12,27 @@ def solution(lottos, win_nums) :
   rank = [6, 6, 5, 4, 3, 2, 1]
   rank_num = []
 
-  for i in lottos :
+  for i in lottos :                    # Compare the numbers of 'lottos' with list of 'win_nums' except 0
     for j in win_nums :
       if i == j :
         minimum_nums += 1
-        j = ''
+        j = ''                         # To avoid duplication in for loop
 
-  for i in lottos :
+  for i in lottos :                    # find 0
     if i == 0 :
       zero += 1
 
-  maximum_nums = minimum_nums + zero
+  maximum_nums = minimum_nums + zero   
 
-  for i in range(0, len(rank)) :
+  for i in range(0, len(rank)) :       # Compare the 'rank' to the 'maximum_nums'
     if maximum_nums == i :
-      rank_num.append(rank[i])
+      rank_num.append(rank[i])         
 
-  for i in range(0, len(rank)) :
+  for i in range(0, len(rank)) :       # Compare the 'rank' to the 'minimum_nums'
      if minimum_nums == i :
        rank_num.append(rank[i]) 
 
   return rank_num
-
 
 lottos = [44, 1, 0, 0, 31, 25]
 win_nums = [31, 10, 45, 1, 6, 19]
