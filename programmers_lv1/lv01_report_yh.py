a854af8b0db63906f6c111b37a80a7280621a010
YHyunCho <yhwmb321@gmail.com>
2022-07-07 13:40:46 +0900

    commit

--- programmers_lv1/lv01_report_yh.py
+++ programmers_lv1/lv01_report_yh.py
@@ -4,53 +4,39 @@
 # 작성자 : 조예현
 # 최초 작성일 : 2022-07-05
 # 최종 작성일 : 2022-07-
+from collections import defaultdict
 
 def solution(id_list, report, k) :
 
-  report_list = []          # List of divided 'report'
-  report_dict = dict()
+  report_list = []  
+  reported_dict = defaultdict(int)     
+  report_dict = defaultdict(set)
   answer = []
 
-  for i in range(len(report)) :
-    for j in range(i+1, len(report)) :
-      if report[i] == report[j] :
-        report[j] = ''
+  report = list(set(report))
 
-  for i in range(len(id_list)):
-    report_dict[id_list[i]] = []
   for i in report :
-    if i == '' :
-      continue
-    report_list.append(i.split())
+    a,b = i.split()
+    report_dict[a].add(b)
+    reported_dict[b] += 1
 
-  report = []
-
-  for j in range(len(report_list)) :
-    num = 1
-    for h in range(len(report_list)) :
-      if j != h and report_list[j][1] == report_list[h][1] :
-        num += 1
-    if num >= k :
-      report.append(report_list[j])
-
-  for i in report_dict.keys() :
-    for j in range(len(report)) :
-      if report[j][0] in i :
-        report_dict[i].append(report[j][1])
-
-  for i in report_dict.keys() :
-    answer.append(len(report_dict[i]))
-
-  return (answer)
+  for i in id_list :
+    result = 0
+    for j in report_dict[i] :
+      if reported_dict[j] >= k :
+        result += 1
+    answer.append(result)
+  
+  return answer
 
 
-# id_list = ["muzi", "frodo", "apeach", "neo"]
-# report = ["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"]
-# k = 2
+id_list = ["muzi", "frodo", "apeach", "neo"]
+report = ["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"]
+k = 2
 
-id_list = ["con", "ryan"]
-report = ["ryan con", "ryan con", "ryan con", "ryan con"]
-k = 3
+# id_list = ["con", "ryan"]
+# report = ["ryan con", "ryan con", "ryan con", "ryan con"]
+# k = 3
 
 result = solution(id_list, report, k)
 print(result)
