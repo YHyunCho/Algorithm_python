bddd36ca3ce1375199308ff54dbdec9179df8b07
YHyunCho <yhwmb321@gmail.com>
2022-07-10 15:41:16 +0900

    Success

--- programmers_lv1/lv01_string_word_yh.py
+++ programmers_lv1/lv01_string_word_yh.py
@@ -9,36 +9,16 @@ def solution(s) :
   eng_number = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
   answer = []
 
-  s_len = len(s)
-
   for i in range(len(eng_number)) :
-    for j in range(len(s)) :
-      if s[j] == '' :
-        break
-      elif s[j].isdigit() :
-        answer.append(s[j])
-        list_s = list(s)
-        list_s[j] = ''
-        s = ''.join(list_s)
-        s = s.ljust(s_len)
-        break
-      else :
-        if s[j:j+2] in eng_number[i] :
-          answer.append(eng_number[i])
-          len_eng = len(eng_number[i]) +j
-          list_s = list(s)
-          list_s[j:j+len_eng] = ''
-          s = ''.join(list_s)
-          s = s.ljust(s_len)
-          break
-    #print(answer)
+    if eng_number[i] in s :
+      s = s.replace(eng_number[i], str(i))
 
-  return answer
+  return int(s)
 
-#s = "one4seveneight"
-s = "one4eightseven"
+s = "one4seveneight"
 # s = "23four5six7"
 # s = "2three45sixseven"
 # s = "123"
 
-answer = solution(s)
+s = solution(s)
+print(s)
