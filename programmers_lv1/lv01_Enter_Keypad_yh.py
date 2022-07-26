83b4d77b088d16f5323c009d6f6be421b79d7af0
YHyunCho <yhwmb321@gmail.com>
2022-07-27 00:10:04 +0900

    키패드 누르기

--- programmers_lv1/lv01_Enter_Keypad_yh.py
+++ programmers_lv1/lv01_Enter_Keypad_yh.py
@@ -1,47 +1,53 @@
+# PROGRAMMERS Lv1 키패드 누르기
+# https://school.programmers.co.kr/learn/courses/30/lessons/67256
+
+# 작성자 : 조예현
+# 최초 작성일 : 2022-07-26
+# 최종 작성일 : 2022-07-26
+
 def solution(numbers, hand):
     answer = ''
-    lastLeftnum = []
-    lastRightnum = []
+    keypad = {1: [0,0], 2: [0,1], 3: [0,2],
+              4: [1,0], 5: [1,1], 6: [1,2],
+              7: [2,0], 8: [2,1], 9: [2,2],
+              '*': [3,0], 0: [3,1], '#': [3,2]}
+    LastEnter = ['*', '#']
 
-    for i in range(len(numbers)) :
+    for i in numbers :
 
-      if numbers[i] == 1 or numbers[i] == 4 or numbers[i] == 7 :
+      if i == 1 or i == 4 or i == 7:
         answer += 'L'
-        lastLeftnum.append(numbers[i])
+        LastEnter[0] = i
 
-      elif numbers[i] == 3 or numbers[i] == 6 or numbers[i] == 9 :
+      elif i == 3 or i == 6 or i == 9 :
         answer += 'R'
-        lastRightnum.append(numbers[i])
-
+        LastEnter[1] = i
+        
       else :
-        leftnum = numbers[i] - lastLeftnum[-1]
-        rightnum = numbers[i] - lastRightnum[-1]
 
-        if leftnum > rightnum :
-          answer += 'R'
-          lastRightnum.append(numbers[i])
+        Left = abs(keypad[LastEnter[0]][0] - keypad[i][0]) + abs(keypad[LastEnter[0]][1] - keypad[i][1])
+        Right = abs(keypad[LastEnter[1]][0] - keypad[i][0]) + abs(keypad[LastEnter[1]][1] - keypad[i][1])
 
-        elif leftnum < rightnum :
+        if Left < Right :
           answer += 'L'
-          lastLeftnum.append(numbers[i]) 
-
+          LastEnter[0] = i
+        elif Left > Right :
+          answer += 'R'
+          LastEnter[1] = i
         else :
           if hand == 'right' :
             answer += 'R'
-            lastRightnum.append(numbers[i])
-
+            LastEnter[1] = i
           else :
             answer += 'L'
-            lastLeftnum.append(numbers[i])
-
-
-    print(lastLeftnum)
-    print(lastRightnum)
-    print(answer)
-
+            LastEnter[0] = i
 
     return answer
 
-numbers = [1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5]
-hand = 'right'
+# numbers = [1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5]
+# hand = 'right'
+numbers = [7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2]
+hand = 'left'
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+# hand = 'right'
 solution(numbers, hand)
