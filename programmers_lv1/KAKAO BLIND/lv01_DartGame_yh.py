bb8c0e9ec3d5757e7a9e03aac3d61c7e66c435f5
YHyunCho <yhwmb321@gmail.com>
2022-08-04 15:49:38 +0900

    다트 게임 미완성

--- programmers_lv1/KAKAO BLIND/lv01_DartGame_yh.py
+++ programmers_lv1/KAKAO BLIND/lv01_DartGame_yh.py
@@ -3,33 +3,65 @@
 
 # 작성자 : 조예현
 # 최초 작성일 : 2022-07-28
-# 최종 작성일 : 2022-07-28
+# 최종 작성일 : 2022-08-03
 
 def solution(dartResult):
     answer = 0
-    dartList = {1: [0, '', ''], 2: [0, '', ''], 3: [0, '', '']}
-    number = []
+    dartList = []
+    number = 0
 
-    L = dartResult.find('10')
-    if L == 0 :
-        dartList[1][0] = 10
-        dartResult = dartResult.replace('10', '--')
-    elif L > 0 and L < 4 :
-        dartList[2][0] = 10
-        dartResult = dartResult.replace('10', '--')
-    elif L > 3 :
-        dartList[3][0] = 10
-        dartResult = dartResult.replace('10', '--')
+    for i in range(len(dartResult)) :
+        if (ord(dartResult[i]) >= 48 and ord(dartResult[i]) <= 57) :
+            if dartResult[i+1] == '0' :
+                number = int(dartResult[i:i+2])
+                dartResult = dartResult.replace(dartResult[i+1], '_')
+                continue
+            else :
+                number = int(dartResult[i])
+                continue
+        
+        if dartResult[i] == 'S' :
+            if dartResult[i] != dartResult[-1] :
+                if (ord(dartResult[i+1]) >= 48 and ord(dartResult[i+1]) <= 57) :
+                    dartList.append(number)
+                    continue
+        elif dartResult[i] == 'D' :
+            number = number ** 2
+            if dartResult[i] != dartResult[-1] :
+                if (ord(dartResult[i+1]) >= 48 and ord(dartResult[i+1]) <= 57) :
+                    dartList.append(number)
+                    continue
+        elif dartResult[i] == 'T' :
+            number = number ** 3
+            if dartResult[i] != dartResult[-1] :
+                if (ord(dartResult[i+1]) >= 48 and ord(dartResult[i+1]) <= 57) :
+                    dartList.append(number)
+                    continue
 
-    
+        if dartResult[i] == '*' :
+            number = number * 2
+            if dartList == '' :
+                dartList.append(number) 
+                continue
+            else :
+                dartList[-1] = dartList[-1] * 2
+                dartList.append(number) 
+                continue
+        elif dartResult[i] == '#' :
+            number = -(number)
+            dartList.append(number)
+            continue
 
+        if i == len(dartResult)-1 :
+            dartList.append(number)
           
-    return answer
+    return sum(dartList)
 
-dartResult = '1S2D*3T'
+# dartResult = '1S2D*3T'
 # dartResult = '1D2S#10S'
 # dartResult = '1D2S0T'
-# dartResult = '1S*2T*3S'
+dartResult = '1S*2T*3S'
 # dartResult = '1D#2S*3S'
 # dartResult = '1T2D3D#'
+# dartResult = '1D2S3T*'
 solution(dartResult)
